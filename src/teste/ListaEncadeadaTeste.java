2c278766ce0032778fe40c7343fcaac481d9b0e6
Matheus Aroxa <matheusaroxa13@gmail.com>
2024-12-07 17:37:46 -0300

    lista encadeada second commit

--- src/teste/ListaEncadeadaTeste.java
+++ src/teste/ListaEncadeadaTeste.java
@@ -1,4 +1,12 @@
 package teste;
 
+import listaencadeada.ListaEncadeada;
+
 public class ListaEncadeadaTeste {
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
+        lista.adiciona(10);
+        System.out.println(lista);
+    }
 }
